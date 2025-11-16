d7a30721b57dca514d4e8c0ceda88b514d406b68
Lazy Cat <yagi.st123@gmail.com>
2025-11-16 09:30:00 +0700

    chore: small changes

--- include/core/value.h
+++ include/core/value.h
@@ -1,11 +1,4 @@
-/**
- * @file value.h
- * @author LazyPaws
- * @brief Core definition of Value in TrangMeo
- * @copyright Copyright (c) 2025 LazyPaws
- * @license All rights reserved. Unauthorized copying of this file, in any form
- * or medium, is strictly prohibited
- */
+// include/core/value.h
 
 #pragma once
 
@@ -14,6 +7,7 @@
 #include "utils/types/variant.h"
 #include "core/meow_object.h"
 #include "core/object_traits.h"
+// KHÔNG #include "core/objects.h" ở đây
 
 namespace meow::core {
 
@@ -25,7 +19,7 @@ template <typename T>
 concept ValueAssignable = std::is_constructible_v<BaseValue, T>;
 
 class Value {
-   private:
+private:
     BaseValue data_;
     [[nodiscard]] inline meow::core::MeowObject* get_object_ptr() const noexcept {
         if (auto obj_ptr = data_.get_if<object_t>()) {
@@ -34,7 +28,7 @@ class Value {
         return nullptr;
     }
 
-   public:
+public:
     // --- Constructors ---
     inline Value() noexcept : data_(null_t{}) {}
     
@@ -78,103 +72,44 @@ class Value {
     }
 
     // --- Kiểu Object (Cụ thể, dùng Type Tag) ---
-    [[nodiscard]] inline bool is_array() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::ARRAY);
-    }
-    [[nodiscard]] inline bool is_string() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::STRING);
-    }
-    [[nodiscard]] inline bool is_hash_table() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::HASH_TABLE);
-    }
-    [[nodiscard]] inline bool is_upvalue() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::UPVALUE);
-    }
-    [[nodiscard]] inline bool is_proto() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::PROTO);
-    }
-    [[nodiscard]] inline bool is_function() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::FUNCTION);
-    }
-    [[nodiscard]] inline bool is_native_fn() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::NATIVE_FN);
-    }
-    [[nodiscard]] inline bool is_class() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::CLASS);
-    }
-    [[nodiscard]] inline bool is_instance() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::INSTANCE);
-    }
-    [[nodiscard]] inline bool is_bound_method() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::BOUND_METHOD);
-    }
-    [[nodiscard]] inline bool is_module() const noexcept {
-        const MeowObject* obj = get_object_ptr();
-        return (obj && obj->get_type() == ObjectType::MODULE);
-    }
+    // *** CHỈ KHAI BÁO (DECLARE) ***
+    [[nodiscard]] inline bool is_array() const noexcept;
+    [[nodiscard]] inline bool is_string() const noexcept;
+    [[nodiscard]] inline bool is_hash_table() const noexcept;
+    [[nodiscard]] inline bool is_upvalue() const noexcept;
+    [[nodiscard]] inline bool is_proto() const noexcept;
+    [[nodiscard]] inline bool is_function() const noexcept;
+    [[nodiscard]] inline bool is_native_fn() const noexcept;
+    [[nodiscard]] inline bool is_class() const noexcept;
+    [[nodiscard]] inline bool is_instance() const noexcept;
+    [[nodiscard]] inline bool is_bound_method() const noexcept;
+    [[nodiscard]] inline bool is_module() const noexcept;
 
 
     // === Các hàm lấy giá trị (Accessors) ===
-    // (Chỉ gọi sau khi đã is_...() - không an toàn)
-
     [[nodiscard]] inline bool as_bool() const noexcept { return data_.get<bool_t>(); }
     [[nodiscard]] inline int64_t as_int() const noexcept { return data_.get<int_t>(); }
     [[nodiscard]] inline double as_float() const noexcept { return data_.get<float_t>(); }
     
-    // Trả về con trỏ cha MeowObject*
     [[nodiscard]] inline meow::core::MeowObject* as_object() const noexcept {
         return data_.get<object_t>();
     }
 
     // Ép kiểu (cast) con trỏ cha thành con trỏ cụ thể
-    // (Rất nhanh, chỉ là static_cast, nhưng phải đảm bảo kiểu đúng)
-    [[nodiscard]] inline array_t as_array() const noexcept {
-        return static_cast<array_t>(as_object());
-    }
-    [[nodiscard]] inline string_t as_string() const noexcept {
-        return static_cast<string_t>(as_object());
-    }
-    [[nodiscard]] inline hash_table_t as_hash_table() const noexcept {
-        return static_cast<hash_table_t>(as_object());
-    }
-    [[nodiscard]] inline upvalue_t as_upvalue() const noexcept {
-        return static_cast<upvalue_t>(as_object());
-    }
-    [[nodiscard]] inline proto_t as_proto() const noexcept {
-        return static_cast<proto_t>(as_object());
-    }
-    [[nodiscard]] inline function_t as_function() const noexcept {
-        return static_cast<function_t>(as_object());
-    }
-    [[nodiscard]] inline native_fn_t as_native_fn() const noexcept {
-        return static_cast<native_fn_t>(as_object());
-    }
-    [[nodiscard]] inline class_t as_class() const noexcept {
-        return static_cast<class_t>(as_object());
-    }
-    [[nodiscard]] inline instance_t as_instance() const noexcept {
-        return static_cast<instance_t>(as_object());
-    }
-    [[nodiscard]] inline bound_method_t as_bound_method() const noexcept {
-        return static_cast<bound_method_t>(as_object());
-    }
-    [[nodiscard]] inline module_t as_module() const noexcept {
-        return static_cast<module_t>(as_object());
-    }
+    // *** CHỈ KHAI BÁO (DECLARE) ***
+    [[nodiscard]] inline array_t as_array() const noexcept;
+    [[nodiscard]] inline string_t as_string() const noexcept;
+    [[nodiscard]] inline hash_table_t as_hash_table() const noexcept;
+    [[nodiscard]] inline upvalue_t as_upvalue() const noexcept;
+    [[nodiscard]] inline proto_t as_proto() const noexcept;
+    [[nodiscard]] inline function_t as_function() const noexcept;
+    [[nodiscard]] inline native_fn_t as_native_fn() const noexcept;
+    [[nodiscard]] inline class_t as_class() const noexcept;
+    [[nodiscard]] inline instance_t as_instance() const noexcept;
+    [[nodiscard]] inline bound_method_t as_bound_method() const noexcept;
+    [[nodiscard]] inline module_t as_module() const noexcept;
 
     // === Các hàm lấy an toàn (Safe Getters) ===
-    // (Tương tự get_if của meow::variant)
-
     [[nodiscard]] inline const bool* as_if_bool() const noexcept { return data_.get_if<bool_t>(); }
     [[nodiscard]] inline const int64_t* as_if_int() const noexcept { return data_.get_if<int_t>(); }
     [[nodiscard]] inline const double* as_if_float() const noexcept { return data_.get_if<float_t>(); }
@@ -183,52 +118,144 @@ class Value {
     [[nodiscard]] inline int64_t* as_if_int() noexcept { return data_.get_if<int_t>(); }
     [[nodiscard]] inline double* as_if_float() noexcept { return data_.get_if<float_t>(); }
 
+    // *** CHỈ KHAI BÁO (DECLARE) ***
     template <typename T>
-    [[nodiscard]] inline T as_if() noexcept {
-        using BaseType = std::remove_pointer_t<T>;
-        
-        if (auto obj = get_object_ptr()) {
-            constexpr ObjectType expected_tag = detail::object_traits<BaseType>::type_tag;
-
-            if (obj->get_type() == expected_tag) {
-                return static_cast<T>(obj);
-            }
-        }
-        return nullptr;
-    }
+    [[nodiscard]] inline T as_if() noexcept;
 
     template <typename T>
-    [[nodiscard]] inline T as_if() const noexcept {
-        using BaseType = std::remove_const_t<std::remove_pointer_t<T>>;
-        
-        if (auto obj = get_object_ptr()) {
-            constexpr ObjectType expected_tag = detail::object_traits<BaseType>::type_tag;
+    [[nodiscard]] inline T as_if() const noexcept;
 
-            if (obj->get_type() == expected_tag) {
-                return static_cast<T>(obj);
-            }
-        }
-        return nullptr;
-    }
     // === Visitor ===
     template <typename Visitor>
     decltype(auto) visit(Visitor&& vis) {
         return data_.visit(vis);
     }
+    // ... (các hàm visit khác) ...
+}; // <-- KẾT THÚC KHAI BÁO CLASS VALUE
 
-    template <typename Visitor>
-    decltype(auto) visit(Visitor&& vis) const {
-        return data_.visit(vis);
-    }
+}  // namespace meow::core // <-- *** ĐÓNG NAMESPACE TẠI ĐÂY ***
 
-    template <typename... Fs>
-    decltype(auto) visit(Fs&&... fs) {
-        return data_.visit(std::forward<Fs>(fs)...);
-    }
 
-    template <typename... Fs>
-    decltype(auto) visit(Fs&&... fs) const {
-        return data_.visit(std::forward<Fs>(fs)...);
+// --- PHÁ VỠ VÒNG LẶP TẠI ĐÂY ---
+// Bây giờ ta bao gồm CÁC ĐỊNH NGHĨA OBJECT ĐẦY ĐỦ
+// (Nó nằm bên ngoài namespace meow::core)
+#include "core/objects.h"
+
+
+// --- *** MỞ LẠI NAMESPACE ĐỂ ĐỊNH NGHĨA CÁC HÀM INLINE *** ---
+namespace meow::core {
+
+// === CÁC HÀM INLINE (BÂY GIỜ MỚI ĐỊNH NGHĨA) ===
+// Trình biên dịch đã có định nghĩa đầy đủ của Value, param_t, VÀ tất cả Obj...
+
+// --- Kiểu Object (Cụ thể, dùng Type Tag) ---
+[[nodiscard]] inline bool Value::is_array() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::ARRAY);
+}
+[[nodiscard]] inline bool Value::is_string() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::STRING);
+}
+[[nodiscard]] inline bool Value::is_hash_table() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::HASH_TABLE);
+}
+[[nodiscard]] inline bool Value::is_upvalue() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::UPVALUE);
+}
+[[nodiscard]] inline bool Value::is_proto() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::PROTO);
+}
+[[nodiscard]] inline bool Value::is_function() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::FUNCTION);
+}
+[[nodiscard]] inline bool Value::is_native_fn() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::NATIVE_FN);
+}
+[[nodiscard]] inline bool Value::is_class() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::CLASS);
+}
+[[nodiscard]] inline bool Value::is_instance() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::INSTANCE);
+}
+[[nodiscard]] inline bool Value::is_bound_method() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::BOUND_METHOD);
+}
+[[nodiscard]] inline bool Value::is_module() const noexcept {
+    const MeowObject* obj = get_object_ptr();
+    return (obj && obj->get_type() == ObjectType::MODULE);
+}
+
+// --- Accessors (Định nghĩa) ---
+[[nodiscard]] inline array_t Value::as_array() const noexcept {
+    return static_cast<array_t>(as_object());
+}
+[[nodiscard]] inline string_t Value::as_string() const noexcept {
+    return static_cast<string_t>(as_object());
+}
+[[nodiscard]] inline hash_table_t Value::as_hash_table() const noexcept {
+    return static_cast<hash_table_t>(as_object());
+}
+[[nodiscard]] inline upvalue_t Value::as_upvalue() const noexcept {
+    return static_cast<upvalue_t>(as_object());
+}
+[[nodiscard]] inline proto_t Value::as_proto() const noexcept {
+    return static_cast<proto_t>(as_object());
+}
+[[nodiscard]] inline function_t Value::as_function() const noexcept {
+    return static_cast<function_t>(as_object());
+}
+[[nodiscard]] inline native_fn_t Value::as_native_fn() const noexcept {
+    return static_cast<native_fn_t>(as_object());
+}
+[[nodiscard]] inline class_t Value::as_class() const noexcept {
+    return static_cast<class_t>(as_object());
+}
+[[nodiscard]] inline instance_t Value::as_instance() const noexcept {
+    return static_cast<instance_t>(as_object());
+}
+[[nodiscard]] inline bound_method_t Value::as_bound_method() const noexcept {
+    return static_cast<bound_method_t>(as_object());
+}
+[[nodiscard]] inline module_t Value::as_module() const noexcept {
+    return static_cast<module_t>(as_object());
+}
+
+// --- Safe Getters (Định nghĩa) ---
+template <typename T>
+[[nodiscard]] inline T Value::as_if() noexcept {
+    using BaseType = std::remove_pointer_t<T>;
+    
+    if (auto obj = get_object_ptr()) {
+        constexpr ObjectType expected_tag = detail::object_traits<BaseType>::type_tag;
+
+        if (obj->get_type() == expected_tag) {
+            return static_cast<T>(obj);
+        }
     }
-};
-}  // namespace meow::core
+    return nullptr;
+}
+
+template <typename T>
+[[nodiscard]] inline T Value::as_if() const noexcept {
+    using BaseType = std::remove_const_t<std::remove_pointer_t<T>>;
+    
+    if (auto obj = get_object_ptr()) {
+        constexpr ObjectType expected_tag = detail::object_traits<BaseType>::type_tag;
+
+        if (obj->get_type() == expected_tag) {
+            return static_cast<T>(obj);
+        }
+    }
+    return nullptr;
+}
+
+}  // namespace meow::core // <-- ĐÓNG NAMESPACE SAU KHI ĐỊNH NGHĨA XONG
